9909329c55429ef0b1b24e6f47b03477ada01c96
Jacqueline Chenault <adigitalnative@gmail.com>
2018-12-18 15:35:17 -0500

    Create 'Sides' entry in book after user create.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  after_create :create_sides_book_item
+
   has_many :auditions
   has_many :projects
   has_many :companies
@@ -10,5 +12,9 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :password_digest, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def create_sides_book_item
+    book_items.create(title: "Prepared Sides")
+  end
 
 end
