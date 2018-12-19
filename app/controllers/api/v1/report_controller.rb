71e4ed04af51fc6874d1efdcddbb02d17a8b75e0
Jacqueline Chenault <adigitalnative@gmail.com>
2018-12-19 10:58:32 -0500

    Remove unused Report CRUD methods from API controller. Were never used.

--- app/controllers/api/v1/report_controller.rb
+++ app/controllers/api/v1/report_controller.rb
@@ -2,23 +2,11 @@ class Api::V1::ReportController < ApplicationController
 
   before_action :set_audition, except: [:result_options]
 
-  def create
-    if @audition.create_report(report_params)
-      render json: @audition, include: ['report', 'report.result'], status: :created
-    end
-  end
-
   def update
     report = @audition.report
     if report.update(report_params)
       render json: @audition, include: ['report', 'report.result'], status: :accepted
     end
-  end
-
-  def destroy
-    report = @audition.report
-    report.destroy
-    render json: @audition, include: ['report', 'report.result'], status: :accepted
   end
 
   def result_options
