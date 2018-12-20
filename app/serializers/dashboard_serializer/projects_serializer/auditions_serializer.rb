8220b1acc6dd372679c0e240b367bbe76a87fb5e
Jacqueline Chenault <adigitalnative@gmail.com>
2018-12-20 15:51:28 -0500

    Dashboard: Serializer tweaks

--- app/serializers/dashboard_serializer/projects_serializer/auditions_serializer.rb
+++ app/serializers/dashboard_serializer/projects_serializer/auditions_serializer.rb
@@ -1,7 +1,17 @@
 class DashboardSerializer::ProjectsSerializer::AuditionsSerializer < ActiveModel::Serializer
-  attributes :id, :date_and_time, :category
+  attributes :id, :date, :category
+
+  has_one :report
 
   def category
     object.category.name
+  end
+
+  def date
+    if object.date_and_time
+      object.date_and_time.strftime('%a, %b %e, %Y')
+    else
+      nil
+    end
   end
 end
