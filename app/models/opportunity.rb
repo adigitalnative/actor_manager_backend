6000202f00e0648b1a8c81d2281fe259c2f60bbf
Jacqueline Chenault <adigitalnative@gmail.com>
2019-01-02 14:21:23 -0500

    Opportunities: Can search for global opportunities

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -6,4 +6,15 @@ class Opportunity < ApplicationRecord
   validates :url, presence: true
   validates :company, presence: true
   validates :active, inclusion: { in: [true, false]}
+
+  # These are not yet tested. Be aware.
+  def self.source_global_opportunities
+    states = State.to_scrape.map {|state| state.name }
+
+    opportunities = Thespis::Runner.search(states)
+    opportunities.each do |opportunity|
+      state = State.find_by_name(opportunity[:state])
+      Opportunity.find_or_create_by(source: opportunity[:source], title: opportunity[:title], company: opportunity[:company], url: opportunity[:url], state: state)
+    end
+  end
 end
